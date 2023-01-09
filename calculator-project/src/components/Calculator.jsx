9b0f9b1b27dcd7b2b89d915f94971272e08a6664
DivyaRayadurgam143 <114801687+DivyaRayadurgam143@users.noreply.github.com>
2023-01-09 19:46:56 +0530

    calculator project

--- calculator-project/src/components/Calculator.jsx
+++ calculator-project/src/components/Calculator.jsx
@@ -17,23 +17,23 @@ export default function Calculator () {
         <br />
         <br />
         <h4>Result is:{result}</h4>
-        <button onClick={() => setInput(input+'1')}>1</button>
-        <button onClick={() => setInput(input+'2')}>2</button>
-        <button onClick={() => setInput(input+'3')}>3</button>
-        <button onClick={() => setInput(input+'4')}>4</button>
-        <button onClick={() => setInput(input+'5')}>5</button><br />
+        <button className='buttons' onClick={() => setInput(input+'1')}>1</button>
+        <button className='buttons' onClick={() => setInput(input+'2')}>2</button>
+        <button  className='buttons' onClick={() => setInput(input+'3')}>3</button>
+        <button className='buttons'  onClick={() => setInput(input+'4')}>4</button>
+        <button className='buttons' onClick={() => setInput(input+'5')}>5</button><br />
         
-        <button onClick={() => setInput(input+'6')}>6</button>
-        <button onClick={() => setInput(input+'7')}>7</button>
-        <button onClick={() => setInput(input+'8')}>8</button>
-        <button onClick={() => setInput(input+'9')}>9</button>
-        <button onClick={() => setInput(input+'0')}>0</button><br />
+        <button className='buttons' onClick={() => setInput(input+'6')}>6</button>
+        <button className='buttons' onClick={() => setInput(input+'7')}>7</button>
+        <button className='buttons' onClick={() => setInput(input+'8')}>8</button>
+        <button className='buttons' onClick={() => setInput(input+'9')}>9</button>
+        <button className='buttons'  onClick={() => setInput(input+'0')}>0</button><br />
 
-        <button onClick={() => setInput(input+'+')}>+</button>
-        <button onClick={() => setInput(input+'-')}>-</button>
-        <button onClick={() => setInput(input+'*')}>*</button>
-        <button onClick={() => setInput(input+'/')}>/</button>
-        <button onClick={() => {setInput('');setResult(0)}}>clr</button><br />
+        <button className='buttons' onClick={() => setInput(input+'+')}>+</button>
+        <button className='buttons' onClick={() => setInput(input+'-')}>-</button>
+        <button className='buttons'  onClick={() => setInput(input+'*')}>*</button>
+        <button className='buttons' onClick={() => setInput(input+'/')}>/</button>
+        <button className='buttons'  onClick={() => {setInput('');setResult(0)}}>clr</button><br />
         <br />
       </center>
 
